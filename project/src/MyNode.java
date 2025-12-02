f8e83d54db4330cd057d2a1437eafa77572e924e
GSTHRW <gustav.herweg@icloud.com>
2025-12-02 14:15:59 +0100

    I will survive

--- project/src/MyNode.java
+++ project/src/MyNode.java
@@ -3,7 +3,7 @@ package tree;
 /**
  * This is a node in a binary search tree. The node holds an integer value.
  * 
- * This is my first change :P
+ * This is my first change :P you cannot change this moamahahaa 
  *
  * @author Torbjorn Ekman
  * @see MyTree
